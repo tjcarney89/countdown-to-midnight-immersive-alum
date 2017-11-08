bd91f42cde261baf7e0a6afe3eb6741fbf442871
TJ Carney <tjcarney@stny.rr.com>
2017-11-08 20:38:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  count = num
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  count = num
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+
 end
